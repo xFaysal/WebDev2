9d809147425b32f5310a125c5ee1620acdbb0b98
Derrick <57630906+Derrick-yup@users.noreply.github.com>
2019-12-04 08:29:25 -0600

    Create webDevUser15_script.js

---
+++ Version5.0/JS/webDevUser15_script.js
@@ -0,0 +1,20 @@
+<html lang="en">
+<!--Version 5.0
+	Name:
+	Date Completed:11-20-19
+ -->
+<head>
+<script src="JS/webDevUser19_script.js"></script>
+<title>WebDevUser15 working</title>
+
+</head>
+
+<body>
+    <center><p id="textChange"> My name is User 15</p></center>
+<center> <button type="button" onclick="changeText()">Click if you like</button>
+
+ </center>
+
+</body>
+
+</html>
