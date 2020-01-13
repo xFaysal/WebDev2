6c5bf5ce4037472769eef610c14958a929d7e9aa
Misteak-541 <57630927+Misteak-541@users.noreply.github.com>
2020-01-13 10:16:06 -0600

    help
    plaese

---
+++ Version8.0/JS/WebDevUser22_script.js
@@ -0,0 +1,140 @@
+
+function myFunction() {
+document.getElementById("myDropdown").classList.toggle("show");
+}
+	
+
+window.onclick = function (e) {
+	if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+
+/*jslint node: true */
+
+
+
+var link = $('link');
+$('#stylebutton .button').on('click', function () {	
+			var $this = $(this);
+			var stylesheet = $(this).data('file');
+			link.attr('href', 'CSS/' + stylesheet + '.css');
+			$this
+			.siblings('button')
+				.removeAttr('disabled')
+				.end()
+				.attr('disabled', 'disabled')
+});
+
+$('#fontchange #initial').on('click', function() {
+		var button1 = document.getElementById("positive");
+		var button2 = document.getElementById("negative");
+		(button1).style.visibility = 'visible';
+		(button2).style.visibility = 'visible';
+});
+$('#fontchange #positive').on('click', function() {
+		var fontSize = parseInt($("body").css("font-size"));
+		fontSize = fontSize + 3 + "px";
+		$("body").css({'font-size':fontSize});
+});
+$('#fontchange #negative').on('click', function() {
+		var fontSize = parseInt($("body").css("font-size"));
+		fontSize = fontSize - 3 + "px";
+		$("body").css({'font-size':fontSize});
+});
+$('#challengebutton #revealer').on('click', function() {
+		var revealer = document.getElementById("revealer");
+		revealer.innerHTML="Are you really?"
+		var button1 = document.getElementById("revealed1");
+		(button1).style.visibility = 'visible';
+		var button2 = document.getElementById("revealed2");
+		(button2).style.visibility = 'visible';
+});
+$('#challengebutton #revealed1').on('click', function() {
+		var x = document.getElementById("textbubble");
+		x.style.visibility = 'visible';
+	});
+$('#challengebutton #revealed2').on('click', function() {
+		var revealer = document.getElementById("revealer");
+		revealer.innerHTML="You up for a challenge?"
+		var button1 = document.getElementById("revealed1");
+		(button1).style.visibility = 'hidden';
+		var button2 = document.getElementById("revealed2");
+		(button2).style.visibility = 'hidden';
+		var x = document.getElementById("textbubble");
+		x.style.visibility = 'hidden';
+});
+
+
+
+
+$('ul li  > a')
+
+$(function() {
+			$('li:first-child').addClass('emphasis');
+	});
+$(document).ready(function() {
+			$('li:first-child').addClass('emphasis');
+		});
+
+//		$(document).(function(){
+//		$("body").css("background-color", "gray");
+//			  });
+// made a comment due to a conflict in objective
+  $(document).ready(function() {
+			var div = document.getElementById("future")
+			var $table = $('<table>');
+				$table.append('<thead>').children('thead')
+		.append('<tr />').children('tr').append('<th>A</th><th>B</th><th>C</th><th>D</th><th>E</th>');
+				$table.append('<tbody />').children('tbody').append('<tr/>').children('tr:last')
+			.append("<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td>")
+			$(div).append($table)
+		});
+
+$(document).ready(function() {
+			$("body").css("font-family", "Comic Sans MS")
+		});
+
+$(document).ready(function() {
+	var $this = document.getElementById("light");
+	$($this).attr('disabled', 'disabled')
+});
+function changeText() {
+ document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
+var space = " ";
+var pos = 0;
+var msg = "User 22";
+
+$(document).ready(function() {
+    var img = document.getElementById("heart");
+    img.style.visibility = 'hidden';
+});
+
+
+function rotate() {
+        var angle = "-90"
+		$(document.getElementById("flipper")).rotateLeft(angle);
+    if ($(document.getElementById("heart")).css("visibility") == "hidden") {
+      var heart = document.getElementById("heart");
+        $(heart).rotateLeft(angle);
+		console.log("rotated hidden");
+		var img = document.getElementById("heart");
+    	img.style.visibility = 'hidden';
+   	} else {
+		var heart = document.getElementById("heart");
+		  $(heart).rotateLeft(angle);
+		  console.log("rotated visable");
+		  heart.style.visibility = 'visible';
+	}
+
+
+ }
+function showImage() {
+    var img = document.getElementById("heart");
+    img.style.visibility = 'visible';
+}
---
+++ Version8.0/PHP/read.php
@@ -0,0 +1,90 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM kanji_ranking WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["kanji"];
+                $address = $row["stroke_count"];
+                $salary = $row["difficulty"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View Record</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Kanji</label>
+                        <p class="form-control-static"><?php echo $row["kanji"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Stroke Count</label>
+                        <p class="form-control-static"><?php echo $row["stroke_count"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Difficulty 1-10</label>
+                        <p class="form-control-static"><?php echo $row["difficulty"]; ?></p>
+                    </div>
+                    <p><a href="../kanji.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
