894b409591824086132b95f37125c10e04c37505
Emmy Dziuk <222382@shakopeeschools.org>
2020-01-21 09:36:17 -0600

    all needed php files

---
+++ Version8.0/user05/create.php
@@ -0,0 +1,129 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $phone = $age = $email ="";
+$name_err = $phone_err = $age_err = $email_err ="";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_phone = trim($_POST["phone"]);
+    if(empty($input_address)){
+        $phone_err = "Please enter your phone number.";     
+    } else{
+        $phone = $input_phone;
+    }
+    
+    // Validate salary
+    $input_age = trim($_POST["age"]);
+    if(empty($input_salary)){
+        $age_err = "Please enter your age.";     
+    } else{
+        $age = $input_age;
+    }
+    
+    $input_email = trim($_POST["email"]);
+    if(empty($input_salary)){
+        $email_err = "Please enter your email.";     
+    } else{
+        $email = $input_email;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($phone_err) && empty($age_err) && empty($email_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_phone, $param_age, $param_email);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_phone = $phone;
+            $param_age = $agee;
+            $param_email = $email;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Create Record</h2>
+                    </div>
+                    <p>Please fill this form and submit to add yourself to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($phone_err)) ? 'has-error' : ''; ?>">
+                            <label>Phone Number</label>
+                            <textarea name="phone" class="form-control"><?php echo $phone; ?></textarea>
+                            <span class="help-block"><?php echo $phone_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($age_err)) ? 'has-error' : ''; ?>">
+                            <label>Age</label>
+                            <input type="text" name="age" class="form-control" value="<?php echo $age; ?>">
+                            <span class="help-block"><?php echo $age_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+                            <span class="help-block"><?php echo $email_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
