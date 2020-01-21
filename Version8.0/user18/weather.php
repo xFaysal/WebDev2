18295fc2f999ff9749745ce656671f1512453630
kineticenergy <228442@shakopeeschools.org>
2020-01-21 08:45:19 -0600

    these are all old changes, ignore them

--- Version8.0/user18/weather.php
+++ Version8.0/user18/weather.php
@@ -12,7 +12,6 @@ else {
 $googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=" . $units . "&APPID=" . $apiKey;
 $googleApiUrl2 = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId2 . "&lang=en&units=" . $units . "&APPID=" . $apiKey;
 $ch = curl_init();
-$ch2 = curl_init();
 
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -21,9 +20,19 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_VERBOSE, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $response = curl_exec($ch);
-
-curl_close($ch);
 $data = json_decode($response);
+curl_close($ch);
+
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch2, CURLOPT_URL, $googleApiUrl2);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch2, CURLOPT_VERBOSE, 0);
+curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+$response1 = curl_exec($ch2);
+curl_close($ch2);
+$data1 = json_decode($response1);
 $currentTime = time();
 $backImage = "http://openweathermap.org/img/w/" . $data->weather[0]->icon. ".png";
 ?>
@@ -32,7 +41,11 @@ $backImage = "http://openweathermap.org/img/w/" . $data->weather[0]->icon. ".png
 <html>
 <head>
 <title>Forecast Weather using OpenWeatherMap with PHP</title>
-
+<link rel="stylesheet" type="text/css" href="CSS/styles.css">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Helvetica">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="JS/script.js"></script>
 <style>
 body {
     font-family: Arial;
@@ -78,7 +91,19 @@ span.min-temperature {
 </head>
     
 <body>
-
+<div id='cssmenu'>
+<ul>
+   <li><a href='index.html'>Home</a></li>
+   <li><a href='global-warming.html'>Global Warming</a></li>
+   <li><a href='climate-change.html'>Climate Change</a></li>
+   <li><a href='problems.html'>Problems</a></li>
+   <li><a href='guestbook.php'>Guest Book</a></li>   
+   <li class='active'><a href='weather.php'>Weather</a></li>
+   <li><a href='solutions.html'>Solutions</a></li>
+   <li><a href='references.html'>References</a></li>
+   <li><a href='contact-us.html'>Contact Us</a></li>
+</ul>
+</div>
     <div class="report-container">
         <h2><?php echo $data->name; ?> Weather Status</h2>
         <div class="time">
@@ -97,7 +122,24 @@ span.min-temperature {
             <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
         </div>
     </div>
-
+<div class="report-container">
+        <h2><?php echo $data1->name; ?> Weather Status</h2>
+        <div class="time">
+            <div><?php echo date("l g:i a", $currentTime); ?></div>
+            <div><?php echo date("jS F, Y",$currentTime); ?></div>
+            <div><?php echo ucwords($data1->weather[0]->description); ?></div>
+        </div>
+        <div class="weather-forecast">
+            <img
+                src="http://openweathermap.org/img/w/<?php echo $data1->weather[0]->icon; ?>.png"
+                class="weather-icon" /> <?php echo $data1->main->temp_max; ?>&deg;<?php echo $temp; ?><span
+                class="min-temperature"><?php echo $data1->main->temp_min; ?>&deg;<?php echo $temp; ?></span>
+        </div>
+        <div class="time">
+            <div>Humidity: <?php echo $data1->main->humidity; ?> %</div>
+            <div>Wind: <?php echo $data1->wind->speed; ?> km/h</div>
+        </div>
+    </div>
 
 </body>
 </html>
