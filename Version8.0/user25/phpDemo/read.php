405eed49aaeb77faaf0943fed2d4b59ea1fd31dd
Lance Suing <34241557+LAsuing@users.noreply.github.com>
2020-01-24 15:36:33 -0600

    Version 8.0

---
+++ Version8.0/user25/phpDemo/read.php
@@ -0,0 +1,91 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM players WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $Player_Name = $row["Player_Name"];
+                $Team = $row["Team"];
+                $Jersey_Number = $row["Jersey_Number"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View Record</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Player_Name</label>
+                        <p class="form-control-static"><?php echo $row["Player_Name"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Team</label>
+                        <p class="form-control-static"><?php echo $row["Team"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Jersey_Number</label>
+                        <p class="form-control-static"><?php echo $row["Jersey_Number"]; ?></p>
+                    </div>
+                    <p><a href="player.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
