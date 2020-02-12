2b01cbe8528106d2fa25bf709439070d7fa625f1
Shakopee22 <60365714+Shakopee22@users.noreply.github.com>
2020-02-12 13:32:16 -0600

    Version 5

---
+++ Version5.0/user24/JS/SampleJS.js
@@ -0,0 +1,48 @@
+function changeText() {
+ document.getElementById("textChange").innerHTML="<div class='Jacob'> Have a good weekend";
+}
+
+var space = " ";
+var pos = 0;
+var msg = "User 19";
+
+function Scroll(){
+document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
+}
+Scroll();
+
+function holaFunction() {
+  document.getElementById("demo").innerHTML = "You selected some text";
+}
+
+function worldFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function whichButton(event) {
+  document.getElementById("demo").innerHTML = event.keyCode;
+}
+
+function loadImage() {
+  alert("Image is loaded");
+}
+
+function myFunction() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
+
+function myFunction() {
+  var w = window.outerWidth;
+  var h = window.outerHeight;
+  var txt = "Window size: width=" + w + ", height=" + h;
+  document.getElementById("demo").innerHTML = txt;
+}
+
+function message() {
+  alert("This alert box was triggered by the onreset event handler");
+}
