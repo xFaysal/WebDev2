aad202464021b2fbe4082ab3d8ef76bf97c5fd30
Radwan <60364211+MonadoBoi420@users.noreply.github.com>
2020-02-13 13:42:29 -0600

    Fixed Bugs
    Finally got button to work

--- Version5.0/user01/JS/SampleJS.js
+++ Version5.0/user01/JS/SampleJS.js
@@ -1,16 +1,17 @@
 function changeText() {
-    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+ document.getElementById("textChange").innerHTML="General Kenobi, a bold one.";
 }
+
 
 var space = " ";
 var pos = 0;
-var msg = "User 1";
+var msg = "User 19";
 
-function Scroll() {
-    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+function Scroll(){
+document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
 
-    pos++;
-    if (pos > msg.length) pos = 0;
-    window.setTimeout("Scroll()", 0);
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
 }
 Scroll();
