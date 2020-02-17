174e145a937cac8cd303287cce4a6ff17b022828
diyathomas <60364054+diyathomas@users.noreply.github.com>
2020-02-17 09:35:56 -0600

    version4

---
+++ Version5.0/user28/JS/SampleJS.js
@@ -0,0 +1,6 @@
+function changeText() {
+ document.getElementById("textChange").innerHTML="<div class= 'diya'> Thanks for liking my Webpage"; 
+}
+function changeTexts() {
+ document.getElementById("textChange").innerHTML="<div class= 'diya'> sry u don't like it"; 
+}
