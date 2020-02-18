71880da0e6de5f04bfeca781ed406af18f4cb728
225550 <60364282+225550@users.noreply.github.com>
2020-02-18 17:11:20 -0600

    new 5.0

--- Version5.0/user19/JS/SampleJS.js
+++ Version5.0/user19/JS/SampleJS.js
@@ -1,3 +1,5 @@
+var texts = 25
+
 function changeText() {
     document.getElementById("textChange").innerHTML = ("Thanks for likeing my Webpage");
 }
@@ -17,14 +19,38 @@ function green() {
 function blue() {
     document.getElementById("textChange").innerHTML = "<div class='blue-b'>Thanks for likeing my Webpage";
 }
+
 function whichButton(event) {
-  document.getElementById("demo").innerHTML = event.keyCode;
+    document.getElementById("demo").innerHTML = event.keyCode;
 }
+
 function show_coords(event) {
-  document.getElementById("demo").innerHTML = "X= " + event.clientX + "<br>Y= " + event.clientY;
+    document.getElementById("demo").innerHTML = "X= " + event.clientX + "<br>Y= " + event.clientY;
 }
+
 function myFun() {
-  document.getElementById("de").innerHTML = "youtube.com/watch?v=jq8-EFzFeyM and https://www.w3schools.com/js/js_events_examples.asp";
+    document.getElementById("de").innerHTML = "youtube.com/watch?v=jq8-EFzFeyM and https://www.w3schools.com/js/js_events_examples.asp";
 }
-function lol(){var msg="are you looking for event #8?";
-return msg;}
+
+function lol() {
+    alert("are you looking for event #1?");
+}
+function tex () {
+  var y = document.getElementById("li");
+  y.value = y.value.toUpperCase();
+    document.getElementById("textChange").innerHTML = ("hi "+y.value);
+
+}
+function bigImg(x) {
+  x.style.height = "180px";
+  x.style.width = "180px";
+}
+
+function normalImg(x) {
+  x.style.height = "100px";
+  x.style.width = "100px";
+
+}
+function data() {
+  document.getElementById("textChange").innerHTML = Date();
+}
