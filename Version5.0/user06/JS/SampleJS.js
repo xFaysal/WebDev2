5f6c39c22bbc8327045bf152a84f40c7438450aa
af307193 <60364138+af307193@users.noreply.github.com>
2020-02-19 12:36:36 -0600

    Frey Version 5
    added functions and games

--- Version5.0/user06/JS/SampleJS.js
+++ Version5.0/user06/JS/SampleJS.js
@@ -1,15 +1,22 @@
 function changeText() {
  document.getElementById("textChange").innerHTML="<div class='ava'> Thanks for liking my Webpage</div>";
 } 
-var space = " ";
-var pos = 0;
-var msg = "User 19";
-
-function Scroll(){
-document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
-
-pos++;
-if (pos > msg.length) pos = 0;
-window.setTimeout("Scroll()", 0);
+function allCaps() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
 }
-Scroll();
+function trigKey() {
+  alert("You pressed a key inside the input field");
+}
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
+function doubleClick() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+function myFunction(elmnt, clr) {
+  elmnt.style.color = clr;
+}
+function whichButton(event) {
+  document.getElementById("demo").innerHTML = event.keyCode;
+}
