572a306ac15d33059a650f7a532740d0280cd186
Joshua <227143@shakopeeschools.org>
2020-02-20 13:38:34 -0600

    Version 5 Script And Games

---
+++ Version5.0/user08/JS/SampleJS.js
@@ -0,0 +1,58 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML = "<div class='terrible'>Thanks for liking my Webpage</div>";
+}
+
+function whichElement(e) {
+    var targ;
+    if (!e) {
+        var e = window.event;
+    }
+    if (e.target) {
+        targ = e.target;
+    } else if (e.srcElement) {
+        targ = e.srcElement;
+    }
+    var tname;
+    tname = targ.tagName;
+    alert("You clicked on a " + tname + " element.");
+}
+
+function changeText2() {
+    document.getElementById("textChange").innerHTML = "<div>My name is JOSHUA HEITZ</div>";
+}
+
+var space = " ";
+var pos = 0;
+var msg = "Welcome To my Java Script WebPage";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
+}
+Scroll();
+
+
+function date() {
+    document.getElementById("p1").innerHTML = Date();
+
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+function myFunction() {
+    var inpObj = document.getElementById("id1");
+    if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+        document.getElementById("demo").innerHTML = "Input OK";
+    }
+}
