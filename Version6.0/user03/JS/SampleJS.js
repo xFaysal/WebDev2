126e0632ca559537e6b4e96b20c75b23bc4af5c0
223131 <60364232+223131@users.noreply.github.com>
2020-02-20 13:52:13 -0600

    Update SampleJS.js

---
+++ Version6.0/user03/JS/SampleJS.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("tableheadtext").innerHTML = "<th>
+                    <center>Month</center>
+                </th>
+                <th>
+                    <center>Rank</center>
+                </th>"
+}
