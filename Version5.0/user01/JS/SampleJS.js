2d75a2664d180717a0ead55e9d8fcce7edaf4e43
Radwan <60364211+MonadoBoi420@users.noreply.github.com>
2020-02-26 00:56:50 -0600

    Elon Musk
    Created The Elon Musk Website

--- Version5.0/user01/JS/SampleJS.js
+++ Version5.0/user01/JS/SampleJS.js
@@ -1,11 +1,16 @@
 function changeText() {
- document.getElementById("textChange").innerHTML="General Kenobi, a bold one.";
+ document.getElementById("textChange").innerHTML="General Kenobi, you are a bold one";
 }
 
-function LoremIpsum() {
-  var x = document.getElementById("fname");
-  x.value = x.value.toUpperCase();
+var space = " ";
+var pos = 0;
+var msg = "User 1";
+
+function Scroll(){
+document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
 }
-
-
-Enter your name: <input type="text" id="fname" onblur="LoremIpsum()">
+Scroll();
