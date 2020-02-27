45250a7fb247fb57f112b6bde392800c7aadb3f1
Emmet <300281@shakopeeschools.org>
2020-02-26 23:49:17 -0600

    put version 7 website together.

---
+++ Version7.0/user10/JS/SampleJS.js
@@ -0,0 +1,18 @@
+function sike() {
+    alert('Is it that surprising that there is no prize?');
+    alert('Now go put in that A++')
+    window.open("https://shakopeemn.infinitecampus.org/campus/portal/students/shakopee.jsp", "_blank");
+
+}
+
+function lightmode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+}
+
+function editwebsite() {
+    alert('Website Edit: Enabled');
+    javascript: document.body.contentEditable = 'true';
+    document.designMode = 'on';
+    void 0
+}
