fecc5a436faca0eb2ac2bca0a7b4223b77181d31
300893 <60364359+300893@users.noreply.github.com>
2020-03-05 14:11:03 -0600

    Update index.php

--- Version8.0/user09/weather/index.php
+++ Version8.0/user09/weather/index.php
@@ -1,5 +1,5 @@
 <?php
-$apiKey = "API KEY"; //You will need to add in the 
+$apiKey = "8081bfd91bf787222bf848c89b48f5f1"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
 $units = "metric";//metric-Celcius  imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 
