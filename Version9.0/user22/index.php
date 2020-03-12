f83306fdccebe69299288d9d204d048f1249bdea
230907 <230907@shakopeeschools.org>
2020-03-12 17:51:47 -0500

    Version 9

---
+++ Version9.0/user22/index.php
@@ -0,0 +1,47 @@
+<?php
+
+php?>
+
+<html lang="en">
+
+<head>
+    <title>Professional Soccer Teams</title>
+    <meta charset="UTF-8">
+    <!-- These are needed to get the responsive menu to work -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="JS/SampleJS.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/SampleCSS.css">
+</head>
+
+<body>
+    <div class="menu">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a href="http://shakonet.isd720.com" class="navbar-brand">WebDev</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <!---------------------------------- Edit These Items in your Menu ------------->
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
+                    <a href="#" class="nav-item nav-link" tabindex="-1">FC Barcelona</a>
+                    <a href="#" class="nav-item nav-link" tabindex="-1"></a>
+                    <a href="#" class="nav-item nav-link" tabindex="-1"></a>
+                    <a href="#" class="nav-item nav-link" tabindex="-1"></a>
+                    <a href="#" class="nav-item nav-link" tabindex="-1"></a>
+                    <!----------------------------------^ Edit These Items in your Menu ^ ------------->
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+</body>
+
+</html>
