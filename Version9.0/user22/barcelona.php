dcbc038d3f2607057160744da9b913a649f67b3f
230907 <230907@shakopeeschools.org>
2020-03-25 10:21:11 -0500

    version 9

--- Version9.0/user22/barcelona.php
+++ Version9.0/user22/barcelona.php
@@ -1,5 +1,42 @@
 <?php
+$apiKey = "e492046fa637c4996e0e5903864807f0"; //You will need to add in the 
+$cityId = "6356055"; //5046997 Shakopee City Id
+$units = "imperial";//metric-Celcius  imperial-Farhenheit
+if ($units == 'metric'){//Changes the $temp varaible to match 
+    $temp = "C";
+}
+else {
+    $temp = "F";
+}
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=" . $units . "&APPID=" . $apiKey;
 
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+
+if(($data->main->temp_min) >= 45){
+    $low='red';
+}
+else {
+    $low='blue';
+}
+
+if (($data->main->temp_max) >= 45){
+    $high='red';
+}
+else {
+    $high='blue';
+}
 ?>
 
 
@@ -17,6 +54,38 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="CSS/SampleCSS.css">
+    <style>
+    .report-container {
+            border: black 3px dashed;
+            padding: 20px 40px 40px 40px;
+            border-radius: 2px;
+            width: 550px;
+            margin: 0 auto;
+            color: black;
+            background-color: aquamarine;
+        }
+
+        .weather-icon {
+            vertical-align: middle;
+            margin-right: 20px;
+        }
+
+        .weather-forecast {
+            color: #212121;
+            font-size: 1.2em;
+            font-weight: bold;
+            margin: 20px 0px;
+        }
+
+        span.min-temperature {
+            margin-left: 15px;
+            color: #929292;
+        }
+
+        .time {
+            line-height: 25px;
+        }
+    </style> 
 </head>
 
 <body class="barcelona">
@@ -37,7 +106,7 @@
                     <a href="#" class="nav-item nav-link" tabindex="-1">Liverpool F.C.</a>
                     <a href="#" class="nav-item nav-link" tabindex="-1">Manchester City F.C.</a>
                     <a href="movies.php" class="nav-item nav-link" tabindex="-1">Soccer Movies</a>
-                    
+
                     <!----------------------------------^ Edit These Items in your Menu ^ ------------->
                 </div>
                 <div class="navbar-nav ml-auto">
@@ -46,7 +115,7 @@
             </div>
         </nav>
     </div>
-    
+
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
@@ -57,8 +126,25 @@
             </div>
         </div>
     </div>
+    <h3>SOCCER WEATHER?</h3>
+    <div class="report-container">
+        <h2><?php echo $data->name; ?> Weather Status</h2>
+        <div class="time">
+            <div><?php echo date("l g:i a", $currentTime); ?></div>
+            <div><?php echo date("jS F, Y",$currentTime); ?></div>
+            <div><?php echo ucwords($data->weather[0]->description); ?></div>
+        </div>
+        <div class="weather-forecast">
+            <img src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png" class="weather-icon" /> <span style="color: <?php echo $high ?>;"><?php echo $data->main->temp_max; ?>&deg;<?php echo $temp; ?></span>
+            <span class="min-temperature" style="color:<?php echo $low ?>;"><?php echo $data->main->temp_min; ?>&deg;<?php echo $temp; ?></span>
+        </div>
+        <div class="time">
+            <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+            <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+        </div>
+    </div>
     
-    
-    
-    </body>
+
+</body>
+
 </html>
