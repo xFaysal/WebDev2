4fc6f7f95e973aec85126a30bf5150519527079e
236860 <60365205+236860@users.noreply.github.com>
2020-04-08 22:55:39 -0500

    Update SampleJS.js

---
+++ Version9.0/user16/JS/SampleJS.js
@@ -0,0 +1,7 @@
+
+   
+    <script language = "javascript"type = "text/javascript" >
+    alert("Welcome to my site") </script> </head> </html>        <font face = "Tahoma" > < a target = "_blank"href =  < span style = "font-size: 8pt; text-decoration: none" > < /span></a > < /font>
+
+
+
