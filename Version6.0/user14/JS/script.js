14dd20ef1d6d75db4c47876fd96ee4ecdfdab7ce
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2021-05-13 14:27:24 -0500

    1.6b 1 Luke

---
+++ Version6.0/user14/JS/script.js
@@ -0,0 +1,16 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
+
+function buttonChange() {
+    document.getElementById("secretMessage").innerHTML = "<b>You got it!</b>";
+}
+
+function changeBackgroundColor(){
+    document.body.style.background = "lime";
+}
+
+function changeTextYell() {
+    document.getElementById("textYell").innerHTML = "<b><strong>I'M ANGRY!!!</strong></b>";
+}
+
