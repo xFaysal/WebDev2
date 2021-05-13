3415fca9fddc6098610102570c5c201be634705a
NoahPeterson <73717263+NoahPeterson@users.noreply.github.com>
2021-05-13 14:17:05 -0500

    1.5

---
+++ Version6.0/user18/JS/script.js
@@ -0,0 +1,18 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML="<u><b>RED HAS KILLED YOU IN ELECTRICAL<b><u>";
+}
+function emergencyMeeting() {
+    document.getElementById("textChange").innerHTML="<u>Who are you voting off the ship?<u>"
+}
+function susssyRed() {
+    document.getElementById("title").innerHTML="test"
+}
+function onBlur() {
+    document.getElementById("fname")
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
+function onFocus() {
+    document.getElementById("name") = ""
+    )
+}
