5c433401af710a436f7abb59335eba5c9ed380ee
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2021-05-14 17:51:19 -0500

    1.6b Luke

--- Version6.0/user14/JS/script.js
+++ Version6.0/user14/JS/script.js
@@ -6,11 +6,27 @@ function buttonChange() {
     document.getElementById("secretMessage").innerHTML = "<b>You got it!</b>";
 }
 
-function changeBackgroundColor(){
+function changeBackgroundColor() {
     document.body.style.background = "lime";
 }
 
 function changeTextYell() {
     document.getElementById("textYell").innerHTML = "<b><strong>I'M ANGRY!!!</strong></b>";
+}
+
+function typeAlert() {
+    alert("I told you not to type!!");
+}
+
+function alertMessage() {
+    alert("This is an alert!!");
+}
+
+function changeColor() {
+    document.getElementById("textColors").style.color="red";
+}
+
+function italicize() {
+    document.getElementById("mouseOut").innerHTML = ("<i>If you leave this, I will italicize!</i>");
 }
 
