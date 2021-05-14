40b4817f3af62feaff3ebfc4313422f9fcfc89e7
maxmeeh <82399362+maxmeeh@users.noreply.github.com>
2021-05-14 14:26:28 -0500

    finished version 6

--- Version6.0/user15/JS/script.js
+++ Version6.0/user15/JS/script.js
@@ -1,37 +1,53 @@
 function changeText() {
-    document.getElementById("textChange").innerHTML="thank you for liking";
-   }
+  document.getElementById("textChange").innerHTML = "thank you for liking";
+}
 
-   function uppercase() {
-       var x = document.getElementById("number two");
-   x.value = x.value.toUpperCase();
-     }
+function uppercase() {
+  var x = document.getElementById("number two");
+  x.value = x.value.toUpperCase();
+}
 
-     function whichElement(e) {
-        var targ;
-        if (!e) {
-          var e = window.event;
-        }
-        if (e.target) {
-          targ=e.target;
-        } else if (e.srcElement) {
-          targ=e.srcElement;
-        }
-        var tname;
-        tname = targ.tagName;
-        alert("You clicked on a " + tname + " element.");
-      }
-      function max(e) {
-        x = e.clientX;
-        y = e.clientY;
-        coor = "Coordinates: (" + x + "," + y + ")";
-        document.getElementById("demo").innerHTML = coor
-      }
-      
-      function clearCoor() {
-        document.getElementById("demo").innerHTML = "";
-      }
+function whichElement(e) {
+  var targ;
+  if (!e) {
+    var e = window.event;
+  }
+  if (e.target) {
+    targ = e.target;
+  } else if (e.srcElement) {
+    targ = e.srcElement;
+  }
+  var tname;
+  tname = targ.tagName;
+  alert("You clicked on a " + tname + " element.");
+}
+function max(e) {
+  x = e.clientX;
+  y = e.clientY;
+  coor = "Coordinates: (" + x + "," + y + ")";
+  document.getElementById("cordinates").innerHTML = coor
+}
 
-      function nice() {
-        document.getElementById("demo").innerHTML = "Hello World";
-      }
+function clearCoor() {
+  document.getElementById("demo").innerHTML = "";
+}
+
+function nice() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function doubleclick() {
+  document.getElementById("hi").innerHTML = "you have solved the puzzle";
+
+}
+
+
+function resize() {
+  var w = window.outerWidth;
+  var h = window.outerHeight;
+  var txt = "Window size: width=" + w + ", height=" + h;
+  document.getElementById("resize").innerHTML = txt;
+}
+function colorchange(elmnt, clr) {
+  elmnt.style.color = clr;
+}
