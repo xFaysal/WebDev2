db07f2cb9d255e99442317554cea960f98955c3e
Robert Mainhardt <rmainhar@shakopee.k12.mn.us>
2021-10-01 07:33:06 -0500

    Update login.php

--- Version8.0/user00/login.php
+++ Version8.0/user00/login.php
@@ -151,7 +151,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
-                <input type="text" name="Ameer" class="form-control" value="<?php echo $username; ?>">
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
